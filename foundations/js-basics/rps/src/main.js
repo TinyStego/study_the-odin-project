de293f551e7c0fc75df1d37073751b960e29ebb3
TinyStego <kyleregants@gmail.com>
2023-04-22 22:51:35 -0700

    add feature to game() to track rounds won and determine final winner

--- foundations/js-basics/rps/src/main.js
+++ foundations/js-basics/rps/src/main.js
@@ -11,29 +11,53 @@ function getRandComputerChoice() {
 }
 
 function playRound(playerChoice, compChoice) {
+  if (playerChoice.toLowerCase() === compChoice) {
+    return `It's a tie! You chose ${playerChoice} and the computer chose ${compChoice}.`;
+  }
+
   for (const key in WINLOSS) {
-    if(playerChoice.toLowerCase() === key &&
+    if (playerChoice.toLowerCase() === key &&
        compChoice === WINLOSS[key]) {
       return `You Win! ${playerChoice} beats ${compChoice}.`;
     }
-    if(compChoice === key &&
+    if (compChoice === key &&
        playerChoice.toLowerCase() === WINLOSS[key]) {
       return `You Lose! ${playerChoice} loses to ${compChoice}.`;
     }
   }
 
-  return `It's a tie! You chose ${playerChoice} and the computer chose ${compChoice}.`;
 }
 
 function game(){
   const rounds = 5;
-  for(let i = 0; i < rounds; ++i){
+
+  let playerScore = 0;
+  let compScore = 0;
+
+  for (let i = 0; i < rounds; ++i){
     let playerChoice = prompt("Choose: Rock, Paper, Scissors");
-    while(!CHOICES.includes(playerChoice.toLowerCase())) {
+    while (!CHOICES.includes(playerChoice.toLowerCase())) {
       playerChoice = prompt(`${playerChoice} is invalid.\nChoose: Rock, Paper, Scissors`);
     }
-
-    const compChoice = getRandComputerChoice();
-    console.log(playRound(playerChoice, compChoice));
+    const compChoice = getRandComputerChoice(); // determine this score later so player
+                                                // can't view it in the debugger
+    const result = playRound(playerChoice, compChoice);
+    if (result.includes("Win")) {
+      ++playerScore;
+    }
+    if (result.includes("Lose")) {
+      ++compScore;
+    }
+    console.log(result);
   } 
+
+  if (playerScore > compScore) {
+    return `You win the game! ${playerScore} to ${compScore}`;
+  }
+  if (playerScore < compScore) {
+    return `You lost the game! ${playerScore} to ${compScore}`;
+  }
+  if (playerScore === compScore) {
+    return `You tied the game! ${playerScore} to ${compScore}`;
+  }
 }
