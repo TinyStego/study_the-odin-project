920907855607fb0c3be4fd74224836bea9cf0b1b
TinyStego <kyleregants@gmail.com>
2023-04-22 22:05:53 -0700

    create function to play a round based on PC and NPC choices

--- foundations/js-basics/rps/src/main.js
+++ foundations/js-basics/rps/src/main.js
@@ -1,7 +1,26 @@
-const CHOICES = ["Rock", "Paper", "Scissors"];
+const CHOICES = ["rock", "paper", "scissors"];
+const WINLOSS = {
+  "paper": "rock",
+  "rock": "scissors",
+  "scissors": "paper"
+}
 
 function getRandComputerChoice() {
   const compChoice = Math.floor(Math.random() * 3);
   return CHOICES[compChoice];
 }
 
+function playRound(playerChoice, compChoice) {
+  for (const key in WINLOSS) {
+    if(playerChoice.toLowerCase() === key &&
+       compChoice === WINLOSS[key]) {
+      return `You Win! ${playerChoice} beats ${compChoice}.`;
+    }
+    if(compChoice === key &&
+       playerChoice.toLowerCase() === WINLOSS[key]) {
+      return `You Lose! ${playerChoice} loses to ${compChoice}.`;
+    }
+  }
+
+  return `It's a tie! You chose ${playerChoice} and the computer chose ${compChoice}.`;
+}
