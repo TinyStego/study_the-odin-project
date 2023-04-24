43d8f1db1a8acc48688c9a925f2f2abed241d549
TinyStego <kyleregants@gmail.com>
2023-04-23 22:24:41 -0700

    fix: create removeFromArray() func and pass first test

--- foundations/js-basics/fundamentals-4/04_removeFromArray/removeFromArray.js
+++ foundations/js-basics/fundamentals-4/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, item) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] !== item) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
