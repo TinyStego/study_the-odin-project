2c4674c68740d4596204c7128da5f42ad5008a36
TinyStego <kyleregants@gmail.com>
2023-04-23 23:29:49 -0700

    refactor: change variable name to better define the object

--- foundations/js-basics/fundamentals-4/05_sumAll/sumAll.js
+++ foundations/js-basics/fundamentals-4/05_sumAll/sumAll.js
@@ -1,9 +1,9 @@
 const sumAll = function(int1, int2) {
     if (Number.isInteger(int1) && Number.isInteger(int2) &&
         int1 >= 0 && int2 >= 0) {
-        const range = getMinMax(int1, int2);
+        const args = getMinMax(int1, int2);
         let sum = 0;
-        for (let i = range.min; i <= range.max; ++i) {
+        for (let i = args.min; i <= args.max; ++i) {
             sum += i;
         }
         return sum;
