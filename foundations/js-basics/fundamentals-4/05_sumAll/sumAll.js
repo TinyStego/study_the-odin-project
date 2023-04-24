f564066d3ee98355120a8353b8dce10d5786abf4
TinyStego <kyleregants@gmail.com>
2023-04-23 23:18:47 -0700

    fix: create sumAll() func and pass first test

--- foundations/js-basics/fundamentals-4/05_sumAll/sumAll.js
+++ foundations/js-basics/fundamentals-4/05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    const range = getMinMax(int1, int2);
+    let sum = 0;
+    for (let i = range.min; i <= range.max; ++i) {
+        sum += i;
+    }
+    return sum;
 };
+
+function getMinMax(int1, int2) {
+    if (int1 < int2) {
+        return { min: int1, max: int2 };
+    }
+    return { min: int2, max: int1 };
+}
 
 // Do not edit below this line
 module.exports = sumAll;
