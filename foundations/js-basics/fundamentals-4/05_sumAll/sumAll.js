a4e6665aa41a1d3d193d36069f89e76271535860
TinyStego <kyleregants@gmail.com>
2023-04-23 23:27:22 -0700

    fix: add check  to verify both arguments are positive ints

--- foundations/js-basics/fundamentals-4/05_sumAll/sumAll.js
+++ foundations/js-basics/fundamentals-4/05_sumAll/sumAll.js
@@ -1,10 +1,15 @@
 const sumAll = function(int1, int2) {
-    const range = getMinMax(int1, int2);
-    let sum = 0;
-    for (let i = range.min; i <= range.max; ++i) {
-        sum += i;
+    if (Number.isInteger(int1) && Number.isInteger(int2) &&
+        int1 >= 0 && int2 >= 0) {
+        const range = getMinMax(int1, int2);
+        let sum = 0;
+        for (let i = range.min; i <= range.max; ++i) {
+            sum += i;
+        }
+        return sum;
     }
-    return sum;
+    
+    return "ERROR";
 };
 
 function getMinMax(int1, int2) {
