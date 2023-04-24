108bdd39f4e2d7931d48e3ca43c7ed6028747064
TinyStego <kyleregants@gmail.com>
2023-04-23 22:48:30 -0700

    fix: add ability to delete multiple items

--- foundations/js-basics/fundamentals-4/04_removeFromArray/removeFromArray.js
+++ foundations/js-basics/fundamentals-4/04_removeFromArray/removeFromArray.js
@@ -1,7 +1,14 @@
-const removeFromArray = function(arr, item) {
+const removeFromArray = function(arr) {
     let newArr = [];
     for (let i = 0; i < arr.length; ++i) {
-        if (arr[i] !== item) {
+        let isFound = false;
+        for (let j = 1; j < arguments.length; ++j) {
+            if (arr[i] === arguments[j]) {
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound) {
             newArr.push(arr[i]);
         }
     }
