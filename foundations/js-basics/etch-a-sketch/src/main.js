ba7503c73d77a3b71c58d8f6e20614cdf0a36fc9
TinyStego <kyleregants@gmail.com>
2023-04-26 11:05:58 -0700

    feat: add buttons to create a new grid of user defined size and clear grid

--- foundations/js-basics/etch-a-sketch/src/main.js
+++ foundations/js-basics/etch-a-sketch/src/main.js
@@ -1,24 +1,42 @@
 const container = document.querySelector(".container");
-const PIXEL_PER_ROW = 16;
-const PIXEL_SIZE = 50;
-const Grid_Size = PIXEL_PER_ROW * PIXEL_SIZE;
 
-container.style.width = `${Grid_Size}px`;
-container.style.height = `${Grid_Size}px`;
-
-for (let i = 0; i < PIXEL_PER_ROW; ++i) {
-    for (let j = 0; j < PIXEL_PER_ROW; ++j) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        container.appendChild(pixel);
+function createGrid() {
+    const pixels = document.querySelectorAll(".pixel");
+    let pixelsPerRow = parseInt(prompt("How many pixels per row? Maximum: 100"));
+    
+    if (Number.isInteger(pixelsPerRow) && pixelsPerRow > 0) {
+        clearGrid(pixels);
+        createPixels(pixelsPerRow);
     }
+}
+
+function createPixels(pixelsPerRow) {
+    const pixelSize = container.clientWidth / pixelsPerRow;
+    console.log(pixelSize);
+    for (let i = 0; i < pixelsPerRow; ++i) {
+        for (let j = 0; j < pixelsPerRow; ++j) {
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            pixel.style.width = `${pixelSize}px`
+            pixel.style.height = `${pixelSize}px`
+            container.appendChild(pixel);
+        }
+    }
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
+    }))
+}
+
+function clearGrid(pixels) {
+    pixels.forEach(pixel => container.removeChild(pixel));
+}
+
+function resetPixels(pixels) {
+    pixels.forEach(pixel => pixel.style.backgroundColor = "white");
 }
 
 function getRandomRGB() {
     return Math.floor(Math.random() * 256);
 }
 
-const pixels = document.querySelectorAll(".pixel");
-pixels.forEach(pixel => pixel.addEventListener("mouseover", () => {
-    pixel.style.backgroundColor = `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
-}))
