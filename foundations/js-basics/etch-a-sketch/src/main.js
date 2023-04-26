691960d355bb893835025539620f200d63c225f0
TinyStego <kyleregants@gmail.com>
2023-04-26 11:16:24 -0700

    fix: prevent createGrid() from running if given improper input

--- foundations/js-basics/etch-a-sketch/src/main.js
+++ foundations/js-basics/etch-a-sketch/src/main.js
@@ -2,17 +2,26 @@ const container = document.querySelector(".container");
 
 function createGrid() {
     const pixels = document.querySelectorAll(".pixel");
-    let pixelsPerRow = parseInt(prompt("How many pixels per row? Maximum: 100"));
+    const userPrompt = prompt("How many pixels per row?\nMinimum: 1 | Maximum: 100");
+    const pixelsPerRow = parseInt(userPrompt);
     
-    if (Number.isInteger(pixelsPerRow) && pixelsPerRow > 0) {
-        clearGrid(pixels);
-        createPixels(pixelsPerRow);
-    }
+    if (!Number.isInteger(pixelsPerRow)) {
+        alert(`Error: ${userPrompt} is not a number.`)
+        return;
+    } else if (pixelsPerRow <= 0) {
+        alert(`Error: ${pixelsPerRow} is less than the minimum.`)
+        return;
+    } else if (pixelsPerRow > 100) {
+        alert(`Error: ${pixelsPerRow} is more than the maximum.`)
+        return;
+    } 
+
+    clearGrid(pixels);
+    createPixels(pixelsPerRow);
 }
 
 function createPixels(pixelsPerRow) {
     const pixelSize = container.clientWidth / pixelsPerRow;
-    console.log(pixelSize);
     for (let i = 0; i < pixelsPerRow; ++i) {
         for (let j = 0; j < pixelsPerRow; ++j) {
             const pixel = document.createElement("div");
