a05f8c8ddc786104118c3f62b645548e967bcaff
TinyStego <kyleregants@gmail.com>
2023-04-27 19:35:32 -0700

    feat: create func to find the oldest person in an array of people

--- foundations/js-basics/fundamentals-5/12_findTheOldest/findTheOldest.js
+++ foundations/js-basics/fundamentals-5/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        return (aAge < bAge) ? 1 : -1;
+    })[0];
 };
 
 // Do not edit below this line
