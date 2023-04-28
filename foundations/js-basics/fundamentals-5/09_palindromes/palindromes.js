dbf46504e0767205b06b8ed053b999038d620dd0
TinyStego <kyleregants@gmail.com>
2023-04-27 19:02:42 -0700

    feat: create func to detect if a sting is a palindrome

--- foundations/js-basics/fundamentals-5/09_palindromes/palindromes.js
+++ foundations/js-basics/fundamentals-5/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    for (let i = 0; i < text.length; ++i) {
+        if (text[i] !== text[text.length - 1 - i]) {
+            return false;
+        }
+    } 
+    return true;
 };
 
 // Do not edit below this line
