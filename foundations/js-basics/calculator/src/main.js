458d0b17a13f29eb3b564526074cbc6e6f15d8f0
TinyStego <kyleregants@gmail.com>
2023-04-28 11:16:38 -0700

    refactor: push values to exp instead of setting the exp[index]

--- foundations/js-basics/calculator/src/main.js
+++ foundations/js-basics/calculator/src/main.js
@@ -41,15 +41,14 @@ function addToDisplay(item) {
 function operator(op) {
     const num = Number(display.value);
     const prev = index - 1;
+    exp[index] = [];
     if (index === 0) {
-        exp[index] = [0,0,0];
-        exp[index][0] = num;
-        exp[index][1] = op;
+        exp[index].push(num);
+        exp[index].push(op);
     } else if (index > 0) {
-        exp[index] = [0,0,0];
-        exp[prev][2] = num;
-        exp[index][0] = operate(exp[prev][1], exp[prev][0], exp[prev][2]);
-        exp[index][1] = op;
+        exp[prev].push(num);
+        exp[index].push(operate(exp[prev][1], exp[prev][0], exp[prev][2]));
+        exp[index].push(op);
     }
     index++;
 }
