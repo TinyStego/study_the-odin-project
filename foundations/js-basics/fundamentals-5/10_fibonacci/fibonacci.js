5c394d08e984e84eaea887386b48e48aba9ef942
TinyStego <kyleregants@gmail.com>
2023-04-27 19:24:03 -0700

    feat: create func to find the nth fibonacci number

--- foundations/js-basics/fundamentals-5/10_fibonacci/fibonacci.js
+++ foundations/js-basics/fundamentals-5/10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (Number.isInteger(num) && num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return num;
+    }
+    let curr = 1;
+    let prev = 0;
+    for (let i = 1; i < +num; ++i) {
+        const temp = prev;
+        prev = curr;
+        curr = temp + curr;
+    }
+    return curr;
 };
 
 // Do not edit below this line
