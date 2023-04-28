8e979e18d5f9f830414e85b502facfa8ca749eb8
TinyStego <kyleregants@gmail.com>
2023-04-27 20:02:54 -0700

    fix: add () to getFullYear to call the function

--- foundations/js-basics/fundamentals-5/12_findTheOldest/findTheOldest.js
+++ foundations/js-basics/fundamentals-5/12_findTheOldest/findTheOldest.js
@@ -1,7 +1,8 @@
 const findTheOldest = function(people) {
-    return people.sort((a, b) => {
-        const aAge = a.yearOfDeath - a.yearOfBirth;
-        const bAge = b.yearOfDeath - b.yearOfBirth;
+    return oldest = people.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        const aAge = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const bAge = (b.yearOfDeath || currentYear) - b.yearOfBirth;
         return (aAge < bAge) ? 1 : -1;
     })[0];
 };
