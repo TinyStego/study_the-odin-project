8f2adde38c946f541de4ceda6af84374598e052a
TinyStego <kyleregants@gmail.com>
2023-04-28 11:06:16 -0700

    feat: calculate and display previous expression when clicking an operator

--- foundations/js-basics/calculator/src/main.js
+++ foundations/js-basics/calculator/src/main.js
@@ -1,8 +1,13 @@
+const operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
-let num1 = 0;
-let num2 = 0;
-let op   = "";
+const exp = [];
+let index = 0;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -21,15 +26,39 @@ function divide(num1, num2) {
 }
 
 function operate(op, num1, num2) {
-    const operations = {
-        "+": add,
-        "-": subtract,
-        "*": multiply,
-        "/": divide
-    }
-    return operations[op](num1, num2);
+    result = operations[op](num1, num2);
+    addToDisplay(result);
+    return result;
 }
 
 function addToDisplay(item) {
-    display.value += item;
+    if (item in operations) {
+        return;
+    }
+    display.value = item;
+}
+
+function operator(op) {
+    const num = Number(display.value);
+    const prev = index - 1;
+    if (index === 0) {
+        exp[index] = [0,0,0];
+        exp[index][0] = num;
+        exp[index][1] = op;
+    } else if (index > 0) {
+        exp[index] = [0,0,0];
+        exp[prev][2] = num;
+        exp[index][0] = operate(exp[prev][1], exp[prev][0], exp[prev][2]);
+        exp[index][1] = op;
+    }
+    index++;
+}
+
+function calculate() {
+    let expression = display.value;
+    if (expression[expression.length - 1] in operations) {
+        return;
+    }
+
+    display.value = test;
 }
