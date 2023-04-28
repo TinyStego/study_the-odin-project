c003d9919f995dbd5d51f8a5bd72a291c5a3f35b
TinyStego <kyleregants@gmail.com>
2023-04-28 12:26:15 -0700

    feat: display result when pressing equal

--- foundations/js-basics/calculator/src/main.js
+++ foundations/js-basics/calculator/src/main.js
@@ -8,6 +8,7 @@ const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 let exp = [];
 let index = 0;
+let pressedEqual = false;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -26,8 +27,9 @@ function divide(num1, num2) {
 }
 
 function operate(op, num1, num2) {
-    result = operations[op](num1, num2);
+    result = Math.round(operations[op](num1, num2));
     addToDisplay(result);
+    console.table(exp);
     return result;
 }
 
@@ -41,19 +43,29 @@ function addToDisplay(item) {
 function operator(op) {
     const num = Number(display.value);
     const prev = index - 1;
-    exp[index] = [];
+    exp[index] = [0,0,0];
     if (index === 0) {
-        exp[index].push(num);
-        exp[index].push(op);
+        exp[index][0] = num;
+        exp[index][1] = op;
     } else if (index > 0) {
-        exp[prev].push(num);
-        exp[index].push(operate(exp[prev][1], exp[prev][0], exp[prev][2]));
-        exp[index].push(op);
+        if (!pressedEqual) {
+            exp[prev][2] = num;
+        }
+        exp[index][0] = operate(exp[prev][1], exp[prev][0], exp[prev][2]);
+        exp[index][1] = op;
     }
     index++;
+    pressedEqual = false;
 }
 
 function calculate() {
+    const num = Number(display.value);
+    const lastExp = index - 1;
+    if(!pressedEqual) {
+        exp[lastExp][2] = num;
+        operate(exp[lastExp][1], exp[lastExp][0], exp[lastExp][2]);
+        pressedEqual = true;
+    }
 }
 
 function clearDisplay() {
